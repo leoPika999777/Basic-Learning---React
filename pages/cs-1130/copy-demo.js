f29f565f53152fd3a6005e9f878fb6c3f06ba242
汪李歐 <147688987+leoPika999777@users.noreply.github.com>
2023-11-30 10:15:17 +0800

    Update copy-demo

--- pages/cs-1130/copy-demo.js
+++ pages/cs-1130/copy-demo.js
@@ -14,22 +14,29 @@ export default function CopyDemo() {
 
   const ac = [1, 2, [3, 4]]
   const ad = [...ac]
+  //只會靠北到一層
   const ae = [ac[0], ac[1], [...ac[2]]]
+  // 深拷貝陣列(很早期也常見的深拷貝語法，有可能不精確)，可以拷貝各層
   const af = JSON.parse(JSON.stringify(ac))
 
   console.log(ac)
   console.log(ad)
+  console.log(ae)
+  console.log(af)
 
   ac[0] = 999
-  // 只會拷貝到一層
 
   console.log(ac)
   console.log(ad)
+  console.log(ae)
+  console.log(af)
 
   ac[2][0] = 888
 
   console.log(ac)
   console.log(ad)
+  console.log(ae)
+  console.log(af)
 
   return (
     <>
