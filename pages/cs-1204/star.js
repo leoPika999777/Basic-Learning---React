4c3790972738d2874b7d9c9dadfc6ba92590381d
汪李歐 <147688987+leoPika999777@users.noreply.github.com>
2023-12-04 13:45:39 +0800

    增加備註

--- pages/cs-1204/star.js
+++ pages/cs-1204/star.js
@@ -12,7 +12,9 @@ export default function Star() {
         {/* 一個陣列 5個成員(5個值)  等於這構造打五個洞  開始塞1 可以在log裡面輸入Array(5).fill(1) 查看  */}
         {Array(5)
           .fill(1)
+           
           .map((v, i) => {
+             // 每個星星的分數，剛好是索引值+1
             const score = i + 1
 
             return (
@@ -20,12 +22,14 @@ export default function Star() {
                 key={i}
                 className={styles['star-btn']}
                 onClick={() => {
+                   // 點按後設定分數
                   setRating(score)
                 }}
               >
                 <span
-                  role="presentation"
                   //分數小於現在的評分  就亮起來
+                  // 判斷分數(score)如果小於等於目前的評分(rating)狀態，則套用亮起樣式
+                  
                   className={score <= rating ? styles['on'] : styles['off']}
                 >
                   &#9733;
