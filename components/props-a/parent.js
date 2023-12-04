64cfcad2a32b2c2c28b294881551c2080116d3c8
汪李歐 <147688987+leoPika999777@users.noreply.github.com>
2023-12-04 15:01:40 +0800

    Props練習 連動Parent&Child

---
+++ components/props-a/parent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Child from './child'
+
+export default function Parent() {
+  return (
+    <>
+      <h2>parent</h2>
+      <Child />
+    </>
+  )
+}
---
+++ pages/cs-1204/props-a.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Parent from '@/components/props-a/parent'
+import Child from '@/components/props-a/child'
+
+export default function PropsA() {
+  return (
+    <>
+      <h1>屬性(Props)範例-A</h1>
+      <Parent />
+    </>
+  )
+}
