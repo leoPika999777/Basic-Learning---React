76d816eeb863f935877712c7ce9199402f170cf3
汪李歐 <147688987+leoPika999777@users.noreply.github.com>
2023-12-04 15:45:42 +0800

    傳值與結構完整寫法

--- components/props-a/parent.js
+++ components/props-a/parent.js
@@ -6,7 +6,14 @@ export default function Parent() {
     <>
       <h2>parent父母元件</h2>
       {/*  父母元件可以用類似HTML屬性給定值的語法，傳遞任何資料給子女元件 */}
-      <Child text="Today We learn React" price={199} hasStock={false} />
+      <Child
+        text="Today We learn React"
+        price={199}
+        hasStock={false}
+        sum={(a, b) => a + b}
+        aa={[1,2,3, 'a']}
+        objA={{ a: 1, b: 2 }}
+      />
     </>
   )
 }
