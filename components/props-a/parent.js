bfa7f6567422832a998d4f56f8dc9d5f81d506a0
汪李歐 <147688987+leoPika999777@users.noreply.github.com>
2023-12-04 15:17:23 +0800

    子女元件可以利用函式的傳入參數props取得父母元件傳來的資料

--- components/props-a/parent.js
+++ components/props-a/parent.js
@@ -4,8 +4,9 @@ import Child from './child'
 export default function Parent() {
   return (
     <>
-      <h2>parent</h2>
-      <Child />
+      <h2>parent父母元件</h2>
+      {/*  父母元件可以用類似HTML屬性給定值的語法，傳遞任何資料給子女元件 */}
+      <Child text="Today We learn React" price={199} hasStock={false} />
     </>
   )
 }
