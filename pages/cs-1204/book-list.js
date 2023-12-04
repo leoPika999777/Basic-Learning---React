8690439ccacc23293723d68eb5a1a353abbdf266
汪李歐 <147688987+leoPika999777@users.noreply.github.com>
2023-12-04 10:22:54 +0800

    booklist 排版

---
+++ pages/cs-1204/book-list.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import Image from 'next/image'
+
+// 範例資料
+import data from '@/data/books.json'
+
+// 實心圖
+import bookmarkIconFill from '@/assets/bookmark-fill.svg'
+// 空心圖
+import bookmarkIcon from '@/assets/bookmark.svg'
+
+function BookList() {
+  // 擴充
+  const initState = data.map((v, i) => {
+    return { ...v, fav: false }
+  })
+
+  const [books, setBooks] = useState(initState)
+
+  return (
+    <>
+      <h1>書籍清單</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ISBN</th>
+            <th>title</th>
+            <th>author</th>
+            <th>加入收藏</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((v, i) => {
+            return (
+              <tr key={v.isbn}>
+                <td>{v.isbn}</td>
+                <td>{v.title}</td>
+                <td>{v.author}</td>
+                <td>
+                  <Image src={bookmarkIcon} alt="" />
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default BookList
--- pages/cs-1204/counter.js
+++ pages/cs-1204/counter.js
@@ -1,30 +1,21 @@
-//http://localhost:3000/cs-1127/counter
-
 import React, { useState } from 'react'
 
 export default function Counter() {
-  // 1-1. 下面是一種宣告  每種宣告的方式不同
-
-  //基本結構用法：const [state, setState] = useState(initialState)
-  // [獲得狀態的變數 ,  設定狀態變數的函式] = useState(初始值)
-  // total 是 得到狀態的變數
-  // setTotal 是設定total的函示
-  // 只有setTotal 可以改變total
-
-  
   const [total, setTotal] = useState(0)
+
   //特定其況下才用   useState(null)
   //完全不用 useState(undefined)  ＆ useState()
-
 
   return (
     <div>
       <h1>{total}</h1>
 
       <button
-        className="test"
+        //加入事件監聽  onClick值必須是一個函式
         onClick={() => {
-          setTotal(total + 1)
+          const newTotal = total + 1
+          setTotal(newTotal)
+          alert('目前計數器數字是' + newTotal)
         }}
       >
         +1
