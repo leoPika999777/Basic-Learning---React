63edae0831db53cc6c2033d33ec7f989e306d27c
汪李歐 <147688987+leoPika999777@users.noreply.github.com>
2023-12-05 10:21:35 +0800

    useEffect處理副作用

--- components/props-b/child-b.js
+++ components/props-b/child-b.js
@@ -1,9 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState , useEffect} from 'react'
 
 // 子女元件，可以利用函式的傳入參數props取得父母元件傳來的資料
 export default function ChildB(props) {
-  console.log(props)
+  //console.log(props)
   const [cData, setCData] = useState('Child B 的內部資料喔')
+
+  //useEffect處理副作用 寫法是直接呼叫
+  //這裡使用是希望子女元件不用靠事件觸發 也可以回送
+  //useEffect(   第一個是函式()=>{} ,  第二個是 陣列[]  )
+  useEffect(()=>{
+    props.setDataFromChild(cData)
+  }, [] )
+
+
+
   return (
     <>
       <h3>子女元件(Child) - B</h3>
