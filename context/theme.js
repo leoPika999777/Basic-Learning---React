ce0999d3b92ccf0ae74269e720f26c3f12590e85
汪李歐 <147688987+leoPika999777@users.noreply.github.com>
2023-12-06 11:56:05 +0800

    context範例-結構_第1與3步

---
+++ context/theme.js
@@ -0,0 +1,4 @@
+// 1. 建立與導出它
+import { createContext } from 'react'
+
+export const ThemeContext = createContext(null)
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-//import '@/styles/globals.css'
+// import '@/styles/globals.css'
 import '@/styles/product-table.css'
+// 3. 最外(上)元件階層包裹提供者元件，讓⽗⺟元件可以提供它:
+import { ThemeContext } from '@/context/theme'
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <ThemeContext.Provider>
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeContext.Provider>
+  )
 }
